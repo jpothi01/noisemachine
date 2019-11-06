4311aa2e5dc02d4ec5c307ae3ef52d961176dadb
John Pothier <john.pothier@outlook.com>
2019-11-05 20:27:56 -0500

    Output only

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate portaudio;
 use portaudio as pa;
 use std::collections::VecDeque;
 use rand::distributions::Distribution;
-use statrs::distribution::{Normal, Uniform};
+use statrs::distribution::{Uniform};
 
 const SAMPLE_RATE: f64 = 44_100.0;
 const CHANNELS: i32 = 2;
@@ -43,13 +43,13 @@ fn run() -> Result<(), pa::Error> {
     let default_host = pa.default_host_api()?;
     println!("default host: {:#?}", pa.host_api_info(default_host));
 
-    let def_input = pa.default_input_device()?;
-    let input_info = pa.device_info(def_input)?;
-    println!("Default input device info: {:#?}", &input_info);
+    // let def_input = pa.default_input_device()?;
+    // let input_info = pa.device_info(def_input)?;
+    // println!("Default input device info: {:#?}", &input_info);
 
-    // Construct the input stream parameters.
-    let latency = input_info.default_low_input_latency;
-    let input_params = pa::StreamParameters::<f32>::new(def_input, CHANNELS, INTERLEAVED, latency);
+    // // Construct the input stream parameters.
+    // let latency = input_info.default_low_input_latency;
+    // let input_params = pa::StreamParameters::<f32>::new(def_input, CHANNELS, INTERLEAVED, latency);
 
     let def_output = pa.default_output_device()?;
     let output_info = pa.device_info(def_output)?;
@@ -60,16 +60,15 @@ fn run() -> Result<(), pa::Error> {
     let output_params =
         pa::StreamParameters::<f32>::new(def_output, CHANNELS, INTERLEAVED, latency);
 
-    // Check that the stream format is supported.
-    pa.is_duplex_format_supported(input_params, output_params, SAMPLE_RATE)?;
-
     // Construct the settings with which we'll open our duplex stream.
-    let settings = pa::DuplexStreamSettings::new(input_params, output_params, SAMPLE_RATE, FRAMES);
+//    let settings = pa::DuplexSettings::new(input_params, output_params, SAMPLE_RATE, FRAMES);
+
+    let settings = pa::stream::OutputSettings::new(output_params, SAMPLE_RATE, FRAMES);
 
     let mut stream = pa.open_blocking_stream(settings)?;
 
     // We'll use this buffer to transfer samples from the input stream to the output stream.
-    let mut buffer: VecDeque<f32> = VecDeque::with_capacity(FRAMES as usize * CHANNELS as usize);
+//    let mut buffer: VecDeque<f32> = VecDeque::with_capacity(FRAMES as usize * CHANNELS as usize);
 
     let mut distribution = Uniform::new(0.0, 0.3).unwrap();
     let mut rng = rand::rngs::ThreadRng::default();
@@ -103,40 +102,52 @@ fn run() -> Result<(), pa::Error> {
     // the output buffer, so watch out for feedback.
     'stream: loop {
         // How many frames are available on the input stream?
-        let in_frames = wait_for_stream(|| stream.read_available(), "Read");
+        // let in_frames = wait_for_stream(|| stream.write_available(), "Write");
 
-        // If there are frames available, let's take them and add them to our buffer.
-        if in_frames > 0 {
-            let input_samples = stream.read(in_frames)?;
-            buffer.extend(input_samples.into_iter());
-            println!("Read {:?} frames from the input stream.", in_frames);
-        }
+        // // If there are frames available, let's take them and add them to our buffer.
+        // if in_frames > 0 {
+        //     let input_samples = stream.read(in_frames)?;
+        //     buffer.extend(input_samples.into_iter());
+        //     println!("Read {:?} frames from the input stream.", in_frames);
+        // }
 
         // How many frames are available for writing on the output stream?
         let out_frames = wait_for_stream(|| stream.write_available(), "Write");
+        if out_frames > 0 {
+            let n_write_samples = out_frames as usize * CHANNELS as usize;
+            stream.write(out_frames, |output| {
+                    for i in 0..n_write_samples {
+                        let generated_sample = generate_sample(&mut distribution, &mut rng) as f32;
+                        previous_output = pink_low_pass(generated_sample, previous_output, 0.1f32);
+                        output[i] = previous_output;
+                    }
+                    println!("Wrote {:?} frames to the output stream.", out_frames);
+                })?;
+        }
 
         // How many frames do we have so far?
-        let buffer_frames = (buffer.len() / CHANNELS as usize) as u32;
+//        let buffer_frames = (buffer.len() / CHANNELS as usize) as u32;
 
         // If there are frames available for writing and we have some to write, then write!
-        if out_frames > 0 && buffer_frames > 0 {
-            // If we have more than enough frames for writing, take them from the start of the buffer.
-            // Otherwise if we have less, just take what we can for now.
-            let write_frames = if buffer_frames >= out_frames {
-                out_frames
-            } else {
-                buffer_frames
-            };
-            let n_write_samples = write_frames as usize * CHANNELS as usize;
+        // if out_frames > 0 && buffer_frames > 0 {
+        //     // If we have more than enough frames for writing, take them from the start of the buffer.
+        //     // Otherwise if we have less, just take what we can for now.
+        //     let write_frames = if buffer_frames >= out_frames {
+        //         out_frames
+        //     } else {
+        //         buffer_frames
+        //     };
+        //     let n_write_samples = write_frames as usize * CHANNELS as usize;
 
-            stream.write(write_frames, |output| {
-                for i in 0..n_write_samples {
-                    let generated_sample = generate_sample(&mut distribution, &mut rng) as f32;
-                    previous_output = pink_low_pass(generated_sample, previous_output, 0.1f32);
-                    output[i] = previous_output;
-                }
-                println!("Wrote {:?} frames to the output stream.", out_frames);
-            })?;
-        }
+        //     stream.write(write_frames, |output| {
+        //         for i in 0..n_write_samples {
+        //             let generated_sample = generate_sample(&mut distribution, &mut rng) as f32;
+        //             previous_output = pink_low_pass(generated_sample, previous_output, 0.1f32);
+        //             output[i] = previous_output;
+        //         }
+        //         println!("Wrote {:?} frames to the output stream.", out_frames);
+        //     })?;
+        // }
+
     }
 }
